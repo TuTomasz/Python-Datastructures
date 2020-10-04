9e3050cb04902d99b4a8e8c26af08faf214c3812
Tomasz Turek <ttomaszito@gmail.com>
2020-10-03 21:01:54 -0400

    added bst file

---
+++ python_datastructures/binarySearchTree.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+    def add(self,value):
+        pass
+    def contains(self,value):
+        pass
+    def remove(self,value):
+        pass
+    def isValid(self):
+        pass
