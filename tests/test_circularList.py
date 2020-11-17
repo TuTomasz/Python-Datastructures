cc6d40b0f236df3f589958d35258c0b18b7662a8
Tomasz Turek <ttomaszito@gmail.com>
2020-11-16 22:15:07 -0500

    updated tests for circular list

--- tests/test_circularList.py
+++ tests/test_circularList.py
@@ -98,10 +98,74 @@ class Test_Queue(unittest.TestCase):
         self.assertEqual(self.circularList.getCurrent(), 3)
 
     def test_getNext(self):
-        pass
+
+        # when list empty
+        self.assertEqual(self.circularList.getNext(), None)
+
+        # if list has 1 element
+        self.circularList.add(5)
+        self.assertEqual(self.circularList.getNext(), 5)
+
+        # if list multiple elements
+        self.circularList.add(1)
+        self.circularList.add(2)
+        self.circularList.add(3)
+        clist = self.circularList
+        first = clist.getNext()
+        second = clist.getNext()
+        third = clist.getNext()
+        fourth = clist.getNext()
+        self.assertEqual(first, 5)
+        self.assertEqual(second, 1)
+        self.assertEqual(third, 2)
+        self.assertEqual(fourth, 3)
+
+        # # when we itterate all the way arround forward getNext()
+        self.circularList.getNext()
+        self.assertEqual(self.circularList.getCurrent(), 5)
+        self.circularList.getNext()
+        self.assertEqual(self.circularList.getCurrent(), 1)
+
+        # # when we itterate towards the back getPrev()
+        self.circularList.getPrevious()
+        self.assertEqual(self.circularList.getCurrent(), 5)
+        self.circularList.getPrevious()
+        self.assertEqual(self.circularList.getCurrent(), 3)
 
     def test_getPrevious(self):
-        pass
+
+        # when list empty
+        self.assertEqual(self.circularList.getPrevious(), None)
+
+        # if list has 1 element
+        self.circularList.add(5)
+        self.assertEqual(self.circularList.getPrevious(), 5)
+
+        # if list multiple elements
+        self.circularList.add(1)
+        self.circularList.add(2)
+        self.circularList.add(3)
+        clist = self.circularList
+        first = clist.getPrevious()
+        second = clist.getPrevious()
+        third = clist.getPrevious()
+        fourth = clist.getPrevious()
+        self.assertEqual(first, 5)
+        self.assertEqual(second, 3)
+        self.assertEqual(third, 2)
+        self.assertEqual(fourth, 1)
+
+        # when we itterate all the way arround forward getNext()
+        self.circularList.getNext()
+        self.assertEqual(self.circularList.getCurrent(), 2)
+        self.circularList.getNext()
+        self.assertEqual(self.circularList.getCurrent(), 3)
+
+        # when we itterate towards the back getPrev()
+        self.circularList.getPrevious()
+        self.assertEqual(self.circularList.getCurrent(), 2)
+        self.circularList.getPrevious()
+        self.assertEqual(self.circularList.getCurrent(), 1)
 
 
 if __name__ == "__main__":
