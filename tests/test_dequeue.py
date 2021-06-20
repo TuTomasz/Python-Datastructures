c4861e6d0a050a9ee220413a3f9afd7af1927d3d
Tomasz Turek <ttomaszito@gmail.com>
2021-06-20 17:34:54 -0400

    added aditional deqeue tests

--- tests/test_dequeue.py
+++ tests/test_dequeue.py
@@ -9,14 +9,41 @@ class Test_DeQueue(unittest.TestCase):
     def test_enqueue(self):
         self.queue.addFirst(2)
         self.assertEqual(self.queue.getSize(), 1)
+        self.queue.addFirst(3)
+        self.assertEqual(self.queue.getSize(), 2)
+        self.assertEqual(self.queue.__str__(), "[3, 2]")
+
+    def test_enqueue_variable(self):
+        self.queue.addFirst(2)
+        self.assertEqual(self.queue.getSize(), 1)
+        self.queue.addFirst(3)
+        self.assertEqual(self.queue.getSize(), 2)
+        self.queue.addLast(4)
+        self.assertEqual(self.queue.__str__(), "[3, 2, 4]")
+
+    def test_enqueue_last(self):
+        self.queue.addLast(2)
+        self.assertEqual(self.queue.getSize(), 1)
         self.queue.addLast(3)
         self.assertEqual(self.queue.getSize(), 2)
+        self.queue.addLast(4)
+        self.assertEqual(self.queue.__str__(), "[2, 3, 4]")
 
     def test_dequeue_(self):
         self.queue.addFirst(3)
         self.assertEqual(self.queue.getSize(), 1)
+        self.queue.addFirst(4)
         self.queue.pollFirst()
-        self.assertEqual(self.queue.getSize(), 0)
+        self.assertEqual(self.queue.getSize(), 1)
+        self.assertEqual(self.queue.__str__(), "[3]")
+
+    def test_dequeue_last(self):
+        self.queue.addLast(3)
+        self.assertEqual(self.queue.getSize(), 1)
+        self.queue.addFirst(4)
+        self.queue.pollLast()
+        self.assertEqual(self.queue.getSize(), 1)
+        self.assertEqual(self.queue.__str__(), "[4]")
 
     def test_isEmpty(self):
         self.assertEqual(self.queue.isEmpty(), True)
